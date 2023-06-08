c9f95986e27c57bfde3c579250457f512402dea1
Barry Paneer <barrypaneer@Barry-MacBook-Pro-4-Working.local>
2023-06-08 20:55:38 +0800

    Removed

--- externality/static/js/src/externality.js
+++ externality/static/js/src/externality.js
@@ -1,42 +1,39 @@
-//$(document).ready(
-$(document).on("hover", ".supported-tags-nav > li",
-    function ExternalContentXBlock(runtime, element) {
-        $(function ($) {
-            /* Here's where you'd do things on page load. */
-        });
+ function ExternalContentXBlock(runtime, element) {
+    $(function ($) {
+        /* Here's where you'd do things on page load. */
+    });
 
-        window.onload = function() {
-            setTimeout(function() {
-                var data = {
-                    'completion': 1.0,
-                };
-                var handlerUrl = runtime.handlerUrl(element, 'publish_completion');
+    window.onload = function() {
+        setTimeout(function() {
+            var data = {
+                'completion': 1.0,
+            };
+            var handlerUrl = runtime.handlerUrl(element, 'publish_completion');
 
-                $.post(handlerUrl, JSON.stringify(data)).complete(function() {});
-            }, 1000);
+            $.post(handlerUrl, JSON.stringify(data)).complete(function() {});
+        }, 1000);
 
-        };
+    };
 
-        function updateResList(activeTab) {
-            var $resources = $(".supported-resources li.resource-item");
-            var activeStatus = activeTab;
+    function updateResList(activeTab) {
+        var $resources = $(".supported-resources li.resource-item");
+        var activeStatus = activeTab;
 
-            $resources.each(function () {
-                if (activeStatus == 'all-tags' || $(this).data('status').split(',').includes(activeStatus) ) {
-                    $(this).show();
-                } else {
-                    $(this).hide();
-                }
-            })
-
-        }
-
-        $('.supported-tags-nav > li', element).bind('click', function() {
-            $(this).find('.btn-link').addClass('active-section');
-            $(this).siblings().find('.btn-link').removeClass('active-section');
-            updateResList($(this).attr('id'));
-
-        });
+        $resources.each(function () {
+            if (activeStatus == 'all-tags' || $(this).data('status').split(',').includes(activeStatus) ) {
+                $(this).show();
+            } else {
+                $(this).hide();
+            }
+        })
 
     }
-)
+
+    $('.supported-tags-nav > li', element).bind('click', function() {
+        $(this).find('.btn-link').addClass('active-section');
+        $(this).siblings().find('.btn-link').removeClass('active-section');
+        updateResList($(this).attr('id'));
+
+    });
+
+}
